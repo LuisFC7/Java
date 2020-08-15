3bbe360265a59239d5a10abc0f235ec2149d0293
LuisFC7 <luisitofc.28@gmail.com>
2020-08-14 19:11:51 -0500

    finished

--- Basic_Calculator/src/Datos.java
+++ Basic_Calculator/src/Datos.java
@@ -3,15 +3,37 @@ public class Datos {
     
     public static void main(String [] args){
         int num1, num2;
+	int decision;
+	int band=1;
         Scanner entrada = new Scanner(System.in);
         System.out.println("*****Basic Calculator*****");
-        System.out.println("Enter number");
-        num1 = entrada.nextInt();
-        System.out.println("Enter number: ");
-        num2 = entrada.nextInt();
-        
-        Operation sum = new Operation();
-        sum.Operation1(num1, num2);
+
+        Operation addition = new Operation();
+	Operation substraction = new Operation();
+	Operation times = new Operation();
+	Operation division = new Operation();
+
+	while(band==1)
+	{	System.out.println("\nEnter number: ");
+		num1 = entrada.nextInt();
+		System.out.println("Enter number: ");
+		num2 = entrada.nextInt();
+		System.out.println("\n");
+		System.out.println("Choose de Operation\n 1. Addition \n 2. Substraction \n 3. Times \n 4. Division \n");
+	
+		decision = entrada.nextInt();
+	if(decision == 1){
+		addition.Operation1(num1, num2);
+	}else if(decision == 2){
+		substraction.Operation2(num1, num2);
+		}else if(decision == 3){
+			times.Operation3(num1, num2);
+			}else if(decision == 4){
+				division.Operation4(num1, num2);
+				}else{ System.out.println("Enter correctly number of operation, please");}
+		System.out.println("Do you want to do another operation? \n 0 = No \n 1 = Yes\n");
+		band = entrada.nextInt();
+	}
     }
-    
+
 }
--- Basic_Calculator/src/Operation.java
+++ Basic_Calculator/src/Operation.java
@@ -1,11 +1,26 @@
 
 public class Operation {
     int num1, num2, result;
-    
+ 	double result1;   
     //methods
     public void Operation1(int num1, int num2){
         result = num1 + num2;
         System.out.println("The result is : "+result);
     }
+
+    public void Operation2(int num1, int num2){
+	    result = num1-num2;
+	    System.out.println("The result is: "+result);
+    }
+
+    public void Operation3(int num1, int num2){
+	    result = num1 * num2;
+	    System.out.println("The result is: "+result);
+    }
     
+    public void Operation4(int num1, int num2){
+	result1=(double)num1 /(double)num2;
+	    System.out.println("The result is: "+result1);
+    }
 }
+
