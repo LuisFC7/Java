593c1aa6a6ed24cc35321e0bc632b2c67b82d193
LuisFC7 <luisitofc.28@gmail.com>
2020-08-18 19:06:01 -0500

    Some changes was done"
    git add .
    
    wñqld
    
    clear

---
+++ Calculadora_Vectorial/Vector3d.java
@@ -0,0 +1,25 @@
+import util.java.Scanner;
+public class Vector3d{
+
+	public void magnitude(){
+		
+		int option;
+		System.out.println("\nDimension of vector: \n1. R2\n2.R3\n");
+	
+		switch(option){
+			case 1: System.out.println("\nEnter coordinates: ");
+			       		
+
+
+		}
+
+	
+	
+	}
+
+
+
+
+
+
+}
