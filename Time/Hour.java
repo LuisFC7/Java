072dfa73921901982d79d38688ccbf249260e43b
LuisFC7 <luisitofc.28@gmail.com>
2020-09-04 18:12:48 -0500

    Two projects was uploaded , but are not finished et

---
+++ Time/Hour.java
@@ -0,0 +1,24 @@
+class Hour{
+	
+	public void Hour(int hour, int minutes, int seconds){
+	int h,m,s;
+		h = hour; 
+		m = minutes;
+		s = seconds;
+		
+
+	}
+
+	public void advance(int h, int m, int s){
+		
+
+
+
+	}
+	
+	
+
+
+
+
+}
---
+++ Time/Principal.java
@@ -0,0 +1,11 @@
+class Principal{
+
+	public static void main(String [] args){
+
+
+
+
+	}
+
+
+}
