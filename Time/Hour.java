94211b56b88bfa194a704271182c5c82d75c3716
LuisFC7 <luisitofc.28@gmail.com>
2020-09-07 20:17:27 -0500

    The application is not working yet

--- Time/Hour.java
+++ Time/Hour.java
@@ -1,23 +1,48 @@
+import java.util.Scanner;
 class Hour{
-	
-	public void Hour(int hour, int minutes, int seconds){
-	int h,m,s;
-		h = hour; 
-		m = minutes;
-		s = seconds;
-		
+	Scanner input = new Scanner(System.in);
+
+	public void Hour(int h, int m, int s){
+		int hour = h;
+		int minute = m;
+		int sec = s;
+
+		//I'm trying to anidade the method
+		option3.savenewdata(hour,minute,sec);
 
 	}
 
 	public void advance(int h, int m, int s){
-		
-
-
+		int more,more2,more3;
+		System.out.println("*****Advance hour*****\n Hours to advance: \n");
+		more = input.nextInt();
+		System.out.prinln("Minutes to advance: \n");
+		more2 = input.nextInt();
+		System.out.println("Secs to advance: \n");
+		more3 = input.nextInt();
+		int newer2 = m+more2;
+		int newer = h+more;
+		int newer3 = s+more3;
+		//I´m looking for a correctly way that works this
+		option1.savenewdata(newer,newer2,newer3);
 
 	}
 	
-	
+	public void savenewdata(int n, int n1, int n2){
+		
+		int nu = n;
+		int ndo = n1;
+		int ntr = n2;
 
+		option2.restart(nu,ndo,ntr);
+
+	}
+	public void print(int h,int m, int s){
+
+		System.out
+
+
+	}
 
 
 
--- Time/Principal.java
+++ Time/Principal.java
@@ -1,9 +1,31 @@
+import java.util.Scanner;
 class Principal{
 
 	public static void main(String [] args){
+	
+	Scanner input = new Scanner(System.in);
+	Hour introduce = new Hour();
+	int hour,minute,seg,option;
+	System.out.println("\n*****Clock*****\n");
+	System.out.println("Enter Hour exactly:\n ");
+	hour = input.nextInt();
+	System.out.println("Enter minute: \n");
+	minute = input.nextInt();
+	System.out.println("Enter sec: \n");
+	seg = input.nextInt();
+	introduce.Hour(hour,minute,seg);
+	System.out.println("1. Advance\n 2. Restart Hour\n 3. Show Hour");
+	option = input.nextInt();
+	switch(option){
+		case 1: introduce.advance();
+			break;
+		case 2: introduce.restart();
+			break;
+		case 3: introduce.print();
+			break;
 
 
-
+	}
 
 	}
 
