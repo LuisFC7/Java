5632f82e33c3f64f534b16caaa447f8569e3b99e
LuisFC7 <luisitofc.28@gmail.com>
2020-09-08 20:23:04 -0500

    Almost is finished, but Now IDK what this program do

--- Time/Hour.java
+++ Time/Hour.java
@@ -1,14 +1,36 @@
 import java.util.Scanner;
 class Hour{
 	Scanner input = new Scanner(System.in);
-
+	//declaration of a new object to give data
+	Hour omenu = new Hour();
+	Hour omenu1 = new Hour();
+	Hour omenu2 = new Hour();
+	Hour omenu3 = new Hour();
+	Hour omenu4 = new Hour();
+	Hour omenu5 = new Hour();
+	Hour omenu6 = new Hour();
+	//constructor to print hour
 	public void Hour(int h, int m, int s){
 		int hour = h;
 		int minute = m;
 		int sec = s;
+		int option;
+		int band =1;
+		System.out.println("1. Advance Hour\n 2.Restart\n 3.Show Current Hour\n");
+		option = input.nextInt();
+
+		switch(option){
+			case 1: omenu.advance(hour,minute,sec);
+				break;
+			case 2:	omenu1.restart(hour,minute,sec); 
+				break;
+			case 3: omenu2.Hour(hour,minute,sec,band);
+				break;
+		
+		}
+
 
 		//I'm trying to anidade the method
-		option3.savenewdata(hour,minute,sec);
 
 	}
 
@@ -16,34 +38,52 @@ class Hour{
 		int more,more2,more3;
 		System.out.println("*****Advance hour*****\n Hours to advance: \n");
 		more = input.nextInt();
-		System.out.prinln("Minutes to advance: \n");
+		System.out.println("Minutes to advance: \n");
 		more2 = input.nextInt();
 		System.out.println("Secs to advance: \n");
 		more3 = input.nextInt();
 		int newer2 = m+more2;
 		int newer = h+more;
 		int newer3 = s+more3;
-		//I´m looking for a correctly way that works this
-		option1.savenewdata(newer,newer2,newer3);
+		//I'm calling a principal constructor to get again main options
+		omenu3.Hour(newer,newer2,newer3);
+		omenu4.datasaver(h,m,s,newer,newer2,newer3);//IDK if is the correct way to do it
 
 	}
-	
-	public void savenewdata(int n, int n1, int n2){
+
+	//now another constructor to print
+	public void Hour(int h, int m, int s,int band){
 		
-		int nu = n;
-		int ndo = n1;
-		int ntr = n2;
-
-		option2.restart(nu,ndo,ntr);
-
-	}
-	public void print(int h,int m, int s){
-
-		System.out
+		int ho = h;
+		int min = m;
+		int se = s;
+		System.out.println(" " +ho+":"+min+":"+se);
 
 
 	}
 
+	public void ghost(int h, int m, int s,int n, int ne, int ne1){
+		int no = h-n;
+		int mo = m-ne;
+		int so = s-ne1;
+		omenu5.restart(no,mo,so);
+	}
+	public void datasaver(int h, int m, int s,int n,int n2,int n3){
+		int hr = h;
+		int mi = m;
+		int sg = s;
+		int cur = n;
+		int cu1 = n2;
+		int cu2 = n3;
+
+		omenu6.ghost(hr,mi,sg,cur,cu1,cu2);
+
+	}
+	public void restart(int h, int m, int s){
+		int hour = h;
+		int minute = m;
+		int sec = s;
+	}
 
 
 }
--- Time/Principal.java
+++ Time/Principal.java
@@ -14,20 +14,7 @@ class Principal{
 	System.out.println("Enter sec: \n");
 	seg = input.nextInt();
 	introduce.Hour(hour,minute,seg);
-	System.out.println("1. Advance\n 2. Restart Hour\n 3. Show Hour");
-	option = input.nextInt();
-	switch(option){
-		case 1: introduce.advance();
-			break;
-		case 2: introduce.restart();
-			break;
-		case 3: introduce.print();
-			break;
-
-
 	}
-
-	}
-
+	
 
 }
