aaf959faf38272f29cdb71a5dfb55ebedd9b4971
LuisFC7 <luisitofc.28@gmail.com>
2020-09-14 19:03:28 -0500

    A new file has been attached, with a method abstract

---
+++ Persona/Person.java
@@ -0,0 +1,29 @@
+//general porpouse
+import java.util.Scanner;
+class Person{
+
+	private String Nombre;
+	private String Direccion;
+	private String Date;
+	private String Sexo;
+	Scanner input = new Scanner(System.in);
+
+	public void Person(String Nombre, String Direccion, String Date, String sexo){
+		
+		String 
+		
+
+
+	}
+
+	public void getData(){
+		String n,add,da,se;
+		System.out.println("\n Name: \n");
+		n = input.nextString();
+
+		
+
+
+	}
+
+}
---
+++ Persona/Principal.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+class Principal{
+
+	public static void main(String []args){
+	
+		Scanner input = new Scanner(System.in);
+		System.out.println("\n*****Enter a member*****\n");
+		int option = input.nextInt();
+
+		switch(option){
+			
+			case 1: //General
+				break;
+
+			case 2: //Estudiantes
+				break;
+
+			case 3: //empleados
+				break;
+
+			case 4: //estudiantes-empleados
+				break;
+
+		}
+
+
+	}
+
+
+
+}
